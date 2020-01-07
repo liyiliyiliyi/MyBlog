86b78c086df30e10efacaf1fb4eeedf41cd3e6d8
10000 <1281853095@qq.com>
2020-01-07 16:44:46 +0800

    tttttt

---
+++ src/filter/RankFilter.java
@@ -0,0 +1,27 @@
+package filter;
+
+import service.ArticleService;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+//@WebFilter(filterName = "RankFilter")
+public class RankFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+        ArticleService as = ArticleService.getInstance();
+        //显示阅读排行
+        req.setAttribute("visit_rank",as.getVisitRank());
+
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
